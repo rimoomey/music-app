7e429665229b7e73d07298d25fe53d3786982a44
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-17 10:24:48 -0600

    make rain animations

--- index.js
+++ index.js
@@ -280,3 +280,46 @@ function playVideo(localHost, favoritedSong) {
 function formatDate(date) {
   return (date === undefined) | (date === "") ? "date not listed" : date;
 }
+
+// ********** raindrop animations ******************
+const rainContainer = document.querySelector("#rain-container");
+
+// background Colors for the raindrop
+const background = [
+  "linear-gradient(transparent, white)",
+  "linear-gradient(transparent, white)",
+  "linear-gradient(transparent, white)",
+  "linear-gradient(transparent, white)",
+  "linear-gradient(transparent, white)"
+];
+
+const amount = 40; // amount of raindops
+let i = 0;
+
+// Looping and creating the raindrop then adding to the rainContainer
+while (i < amount) {
+  //  Creating and Element
+  const drop = document.createElement("i");
+
+  //   CSS Properties for raindrop
+  const raindropProperties = {
+    width: Math.random() * 5 + "px",
+    positionX: Math.floor(Math.random() * window.innerWidth) + "px",
+    delay: Math.random() * -50 + "s",
+    duration: Math.random() * 20 + "s",
+    bg: background[Math.floor(Math.random() * background.length)],
+    opacity: Math.random() + 0.2
+  };
+
+  //   Setting Styles for raindrop
+  drop.style.width = raindropProperties.width;
+  drop.style.left = raindropProperties.positionX;
+  drop.style.animationDelay = raindropProperties.delay;
+  drop.style.animationDuration = raindropProperties.duration;
+  drop.style.background = raindropProperties.bg;
+  drop.style.opacity = raindropProperties.opacity;
+
+  //   Appending the raindrop in the raindrop container
+  rainContainer.appendChild(drop);
+  i++;
+}
